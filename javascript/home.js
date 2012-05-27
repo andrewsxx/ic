e6262e205b273d2231ffada52df08a1987f26f3a
Andres <ayn_nian@hotmail.com>
2012-05-27 17:31:45 -0500

    propuesta de andres

--- javascript/home.js
+++ javascript/home.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
 //    alert("hei");
 //    $('.relieve-container').imageFrame('sharp');
+
+    $(".test-fondo a").click(function(){
+        var fondo = $(this).attr("fondo");
+        fondo++;
+        if(fondo == 6) fondo=1;
+        switch (fondo){
+            case 1: 
+                $("body").css("background", "transparent url(../images/fondo-andres.png) repeat-x");
+                break;
+            default: 
+                $("body").css("background", "transparent url(../images/fondo-andres" + fondo + ".png) repeat-x");
+                break;
+        }
+        $(".fondo-numero").html(fondo);
+        $(this).attr("fondo", fondo);
+    });
 });
